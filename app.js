013c8c3ebaa6d6b09902ce0d6643eec2aa653dcd
iamtylerd <tylerdanielphotography@gmail.com>
2016-04-22 14:36:22 -0500

    fixed all syntax and auto response errors

--- app.js
+++ app.js
@@ -45,9 +45,11 @@ client.on('chat', function(channel, user, message, self) {
 	if(message === "!brandon") {
 		client.action("eDONK", 'Quote from Brandon, "I almost had sex with a tranny, she tricked me in the club, got her home to finally realize she was a dude.  I almost did it anyways."');	
 	};
-	if(message === "!robsbrobot") {
+	if(message === "!robsbrobot") {	
 		client.action('eDONK', user['display-name'] + " Please address me as Sir eDONK king of the North!");
+		};
 		});
+
 
 client.on('connected', function(address, port) {
 	client.action("eDONK", "Welcome Bros, I am robsbrobot, check out this app I made http://bit.ly/1rqkkwQ");
